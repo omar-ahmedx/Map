6f150b46d0b678430cf408e09eae47c07f9eb173
omar <omerx4444@gmail.com>
2020-12-19 03:08:10 +0200

    additional functionality

--- main.js
+++ main.js
@@ -37,5 +37,108 @@ map.on("load", function () {
 		label.setAttribute("for", layers[i].id);
 		label.textContent = layers[i].id;
 		filterGroup.appendChild(label);
+
+		var item = document.createElement("div");
+		var key = document.createElement("span");
+		key.className = "legend-key";
+		key.style.backgroundColor = layers[i].paint["circle-color"];
+
+		var value = document.createElement("span");
+		value.innerHTML = layers[i].id;
+		item.appendChild(key);
+		item.appendChild(value);
+		legend.appendChild(item);
 	}
+});
+
+map.on("click", function (e) {
+	var features = map.queryRenderedFeatures(e.point, {
+		layers: [
+			"Malls",
+			"Restaurants And Cafes",
+			"Museum And Cultural Centers",
+			"Show And Performance",
+			"Sport",
+			"Gallery",
+			"Hotels",
+			"Festival",
+			"Games",
+			"Educational And Art",
+			"Diriyah Season",
+			"Cinema",
+		],
+	});
+
+	if (!features.length) {
+		return;
+	}
+
+	var feature = features[0];
+	console.log(feature.properties);
+	let image, description, date, time, targetAud;
+	if (feature.properties.Image == undefined || feature.properties.Image == "") {
+		image = "";
+	} else {
+		image = `<img src=${feature.properties.Image} width="200px; height="200px">`;
+	}
+
+	if (
+		feature.properties.Descriptio == undefined ||
+		feature.properties.Descriptio == ""
+	) {
+		description = "";
+	} else {
+		description = `<p>${feature.properties.Descriptio}</p>`;
+	}
+
+	if (feature.properties.Date == undefined || feature.properties.Date == "") {
+		date = "";
+	} else {
+		date = `<p>${feature.properties.date}</p>`;
+	}
+
+	if (feature.properties.Time == undefined || feature.properties.Time == "") {
+		time = "";
+	} else {
+		time = `<p>${feature.properties.Time}</p>`;
+	}
+
+	if (
+		feature.properties.Target_aud == undefined ||
+		feature.properties.Target_aud == ""
+	) {
+		targetAud = "";
+	} else {
+		targetAud = `<p>${feature.properties.Target_aud}</p>`;
+	}
+
+	var popup = new mapboxgl.Popup({ offset: [0, 0] })
+		.setLngLat(feature.geometry.coordinates)
+		.setHTML(
+			image +
+				"<h3>" +
+				feature.properties.Name +
+				"</h3>" +
+				description +
+				time +
+				targetAud +
+				`<a href=${feature.properties.Website} target="_blank">` +
+				"Website" +
+				"</a>"
+		)
+		.addTo(map);
+});
+
+const legendSlider = document.querySelector("#legend-slider");
+legendSlider.addEventListener("click", () => {
+	const legend = document.querySelector("#legend");
+	legend.classList.toggle("legend-slide");
+	legendSlider.classList.toggle("slider-arrow");
+});
+
+const filterSlider = document.querySelector("#filter-slider");
+filterSlider.addEventListener("click", () => {
+	const filter = document.querySelector("#filter-group");
+	filter.classList.toggle("filter-slide");
+	filterSlider.classList.toggle("slider-arrow");
 });
